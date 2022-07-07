e1ca433cc8f5c17d1d82645e53b54312a754eef6
RhonaJoyKe <rhonajoy8@gmail.com>
2022-07-07 13:23:21 -0400

    commit tree 0a993c50b1a8c70b15b84296055bd748ae6cc784

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function wrapAdjective(result='*'){
+    return function(emphatic="special"){
+        return `You are ${result} ${emphatic}`
+    }
+}
+wrapAdjective("*")("a hard worker");
+wrapAdjective("||")("a dedicated programmer");
